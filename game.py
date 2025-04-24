0924f07e88a5329b9b2ff8d3bb3dcacecaa7a044
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-04-24 21:00:44 +0200

    Add collision

--- game.py
+++ game.py
@@ -4,12 +4,24 @@ from window import Window
 from object import Object
 from time import sleep
 
+objects = [
+    Object(1, 1, "O"),
+    Object(2, 2, "X"),
+    Object(3, 3, "Y"),
+    Object(4, 4, "Z"),
+    Object(5, 5, "A"),
+    Object(6, 6, "B"),
+    Object(7, 7, "C"),
+    Object(8, 8, "D"),
+]  # List to hold all objects in the game
+
 
 class Game:
     def __init__(self, stdscr):
         self.player = Player(5, 5, 100)
         self.window = Window(stdscr)
         self.running = True
+        self.score = 0
 
     def run(self):
         """Main game loop"""
@@ -17,7 +29,10 @@ class Game:
             keypress = self.window.get_input()
             self.handle_input(keypress)
             self.window.screen_clear()
+            self.window.draw_list(objects)
             self.window.draw_player(self.player.pos_x, self.player.pos_y)
+            self.check_collision()
+            self.window.stdscr.addstr(0, 0, f"Score: {self.score}")
             self.window.screen_refresh()
 
             sleep(0.05)  # Add a small delay to control the frame rate
@@ -37,3 +52,14 @@ class Game:
         elif keypress == ord("d"):
             if self.player.pos_x < self.window.max_width - 1:
                 self.player.pos_x += 1
+
+    # Check for collision with objects
+    def check_collision(self):
+        for object in objects:
+            if (
+                self.player.pos_x == object.pos_x
+                and self.player.pos_y == object.pos_y
+            ):
+                self.score += 1
+                objects.remove(object)
+                break
